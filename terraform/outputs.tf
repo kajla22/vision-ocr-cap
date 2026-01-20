a0ef8646906eb4b63559e752d7b7595668093bc5
kajla22 <abhinavkajla2208@gmail.com>
2026-01-20 18:11:48 +0530

    teraaform commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,14 @@
-output "resource_group" {
-  value = azurerm_resource_group.rg.name
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  description = "Storage account name"
+  value       = azurerm_storage_account.storage.name
+}
+
+output "storage_account_id" {
+  description = "Storage account ID"
+  value       = azurerm_storage_account.storage.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,20 @@
+variable "resource_group_name" {
+  description = "6517-rg"
+  type        = string
+}
+
 variable "location" {
-  default = "South India"
+  description = "Azure region"
+  type        = string
+  default     = "South India"
+}
+
+variable "storage_account_name" {
+  description = "6517storage1"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "6517-func-app"
+  type        = string
 }
